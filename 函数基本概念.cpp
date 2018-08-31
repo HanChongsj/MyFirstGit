20dd2e0dee7456ace8e66f9ccc848348a84dfdcb
your name <your email>
2018-08-31 19:36:01 +0800

    2

---
+++ 函数基本概念.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	char a[10];
+	cin>>a;
+	cout<<a<<endl;
+    cout<<strlen(a)<<endl;
+
+	system("pause");
+	return 0;
+
+
+
+}
